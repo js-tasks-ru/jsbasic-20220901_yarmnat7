7c8110bb2abca500e4e959508ed091b853ff6cd6
NatYarm <yarmnat7@gmail.com>
2022-09-20 10:58:32 +0300

    Module-4 Tasks completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const fullName = friends.map((friend) => {
+    return [`${friend.firstName} ${friend.lastName}`];
+  });
+
+  const ul = document.createElement("ul");
+  ul.classList.add("friends-list");
+  for (i = 0; i < friends.length; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = `${fullName[i]}`;
+    ul.appendChild(li);
+  }
+  return ul;
 }

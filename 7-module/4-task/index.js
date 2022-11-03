5a76752ba90b880a7a0db00543baa963aff0a501
NatYarm <yarmnat7@gmail.com>
2022-11-03 12:57:52 +0300

    Corrected the slider and made the required changes

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -3,77 +3,86 @@ import createElement from "../../assets/lib/create-element.js";
 export default class StepSlider {
   elem = null;
 
-  constructor({ steps, value }) {
+  constructor({ steps, value = 0 }) {
     this.steps = steps;
-    this.value = value;
+    this.segments = steps - 1;
+
     this.#render();
+    this.setValue(value);
+    this.addEventListeners();
   }
 
   #render() {
     this.elem = createElement(this.#template());
-    this.#createSpans();
-
-    this.elem.addEventListener("click", this.#onClick);
-
-    let thumb = this.elem.querySelector(".slider__thumb");
-    thumb.ondragstart = (e) => {
-      e.preventDefault();
-    };
-
-    thumb.addEventListener("pointerdown", this.#onPointerDown);
   }
 
-  #onClick = (e) => {
-    // Calculating step
-    const left = e.clientX - this.elem.getBoundingClientRect().left;
-    const leftRelative = left / this.elem.offsetWidth;
-    const segments = this.steps - 1;
-    const value = Math.round(leftRelative * segments);
+  setValue(value) {
+    this.value = value;
 
     // Displaying step change
-    let thumb = this.elem.querySelector(".slider__thumb");
-    let progress = this.elem.querySelector(".slider__progress");
-    let leftPercents = (value / segments) * 100;
+    const thumb = this.elem.querySelector(".slider__thumb");
+    const progress = this.elem.querySelector(".slider__progress");
+    let leftPercents = (this.value / this.segments) * 100;
+
     thumb.style.left = `${leftPercents}%`;
     progress.style.width = `${leftPercents}%`;
-
-    const sliderValue = this.elem.querySelector(".slider__value");
-    sliderValue.innerText = value;
+    this.elem.querySelector(".slider__value").innerText = this.value;
 
     //Removing and adding step__active class
     const spans = Array.from(
       this.elem.querySelector(".slider__steps").children
     );
     spans.map((span) => span.classList.remove("slider__step-active"));
-    const activeSpan = spans.at(value);
-    activeSpan.classList.add("slider__step-active");
+    spans.at(this.value).classList.add("slider__step-active");
+  }
+
+  addEventListeners() {
+    let thumb = this.elem.querySelector(".slider__thumb");
+
+    thumb.ondragstart = (e) => {
+      e.preventDefault();
+    };
+    thumb.onpointerdown = this.#onPointerDown;
+    this.elem.onclick = this.#onClick;
+  }
+
+  #onClick = (e) => {
+    // Calculating step
+    let left =
+      (e.clientX - this.elem.getBoundingClientRect().left) /
+      this.elem.offsetWidth;
+
+    this.setValue(Math.round(left * this.segments));
 
     // Custom event
-    const event = new CustomEvent("slider-change", {
-      detail: value,
-      bubbles: true,
-    });
-
-    this.elem.dispatchEvent(event);
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
   };
 
-  #onPointerDown = () => {
+  #onPointerDown = (e) => {
+    e.preventDefault();
     document.addEventListener("pointermove", this.#onPointerMove);
     document.addEventListener("pointerup", this.#onPointerUp, { once: true });
   };
 
   #onPointerMove = (e) => {
+    e.preventDefault();
     // Calculating step
-    const left = e.clientX - this.elem.getBoundingClientRect().left;
-    let leftRelative = left / this.elem.offsetWidth;
+    let left =
+      (e.clientX - this.elem.getBoundingClientRect().left) /
+      this.elem.offsetWidth;
 
-    if (leftRelative < 0) {
-      leftRelative = 0;
+    if (left < 0) {
+      left = 0;
     }
-    if (leftRelative > 1) {
-      leftRelative = 1;
+    if (left > 1) {
+      left = 1;
     }
-    let leftPercents = leftRelative * 100;
+    let leftPercents = left * 100;
 
     // Displaying step change
     let thumb = this.elem.querySelector(".slider__thumb");
@@ -82,44 +91,33 @@ export default class StepSlider {
     progress.style.width = `${leftPercents}%`;
 
     // Changing value
-    const segments = this.steps - 1;
-    const value = Math.round(leftRelative * segments);
-    let sliderValue = this.elem.querySelector(".slider__value");
-    sliderValue.innerText = value;
+    this.value = Math.round(left * this.segments);
+    this.elem.querySelector(".slider__value").innerText = this.value;
 
     //Removing and adding step__active class
     const spans = Array.from(
       this.elem.querySelector(".slider__steps").children
     );
     spans.map((span) => span.classList.remove("slider__step-active"));
-    const activeSpan = spans.at(value);
-    activeSpan.classList.add("slider__step-active");
+    spans.at(this.value).classList.add("slider__step-active");
 
     this.elem.classList.add("slider_dragging");
   };
 
   #onPointerUp = () => {
-    const sliderValue = this.elem.querySelector(".slider__value");
-    let value = +sliderValue.innerText;
-
-    const event = new CustomEvent("slider-change", {
-      detail: value,
-      bubbles: true,
-    });
-
-    this.elem.dispatchEvent(event);
-
     document.removeEventListener("pointermove", this.#onPointerMove);
+    document.removeEventListener("pointerup", this.#onPointerUp);
     this.elem.classList.remove("slider_dragging");
-  };
 
-  #createSpans = () => {
-    for (let i = 0; i < this.steps; i++) {
-      const span = document.createElement("span");
-      const sliderSteps = this.elem.querySelector(".slider__steps");
-      sliderSteps.append(span);
-      sliderSteps.firstElementChild.classList.add("slider__step-active");
-    }
+    const sliderValue = this.elem.querySelector(".slider__value");
+    this.value = +sliderValue.innerText;
+
+    this.elem.dispatchEvent(
+      new CustomEvent("slider-change", {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
   };
 
   #template() {
@@ -130,6 +128,7 @@ export default class StepSlider {
     </div>
     <div class="slider__progress"></div>
     <div class="slider__steps">
+    ${"<span></span>".repeat(this.steps)}
     </div>
   </div>
     `;
